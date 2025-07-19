fb32c324fc8d90fec0ddd1cee9bb8e7b21e101c1
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2025-07-19 19:04:34 +0300

    fix outfile format

--- project4/hashcat.sh
+++ project4/hashcat.sh
@@ -2,7 +2,7 @@ hashcat -m 500 -a 0 -O \
   -r rules/T0XlC.rule \
   --session=toxic \
   --status \
-  --status-timer=60 \
+  --status-timer=300 \
   --outfile=hashcat-cracked.txt \
-  --outfile-format=2 \
+  --outfile-format=1 \
   remaining.hash wordlist.txt
