70ace8e46a517ec2648ae50145b502a218f3fb16
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2025-07-21 02:47:28 +0300

    better

--- project4/hashcat.sh
+++
@@ -1,8 +0,0 @@
-hashcat -m 500 -a 0 -O \
-  -r rules/T0XlCv2.rule \
-  --session=toxicv2 \
-  --status \
-  --status-timer=300 \
-  --outfile=hashcat-cracked.txt \
-  --outfile-format=1 \
-  remaining.hash wordlist.txt
