c807831e78176bec2ea3508bb9f3e75626ca755d
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2025-07-21 03:27:54 +0300

    script edited

--- project4/general.sh
+++
@@ -1,3 +0,0 @@
-hashcat -m 500 -O -a 3 remaining.hash '?d?l?l?l?l?l?l?d?d?d' --session=brute10 \
-  --status --status-timer=60 --force --remove \
-  --outfile=hashcat-cracked.txt --outfile-format=3
--- project4/incrementaljohn.sh
+++
@@ -1,4 +0,0 @@
-john --mask='?d?l?l?l?l?l?l?l?d?d' \
-     --format=md5crypt \
-     --fork=6 \
-     remaining.shadow
